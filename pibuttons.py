2b8c2c57811cf9169c89cc636315e3e86f476f55
Austin Sloane <Blaradox@users.noreply.github.com>
2017-08-09 19:18:54 -0700

    added reboot function

--- pibuttons.py
+++ pibuttons.py
@@ -7,25 +7,21 @@ import time
 import os
 
 # Use the Broadcom SOC Pin numbers 
-shtdwn = 21
-bltth = 26
-
+button = 21
 
 # Setup the Pin with Internal pullups enabled and PIN in reading mode. 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(shtdwn, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-GPIO.setup(bltth, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(button, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
 # Our functions on what to do when the buttons are pressed
-def Shutdown(channel):
+def shutdown():
 	os.system("sudo shutdown -h now")
-
-def Bluetooth(channel):
-	os.system("/home/pi/Scripts/bluetooth.sh")
+def reboot(): 
+	os.system("sudo shutdown -r now")
 
 # Add our functions to execute when the button pressed event happens
-GPIO.add_event_detect(shtdwn, GPIO.FALLING, callback = Shutdown, bouncetime = 2000)
-GPIO.add_event_detect(bltth, GPIO.FALLING, callback = Bluetooth, bouncetime = 2000)
+GPIO.add_event_detect(button, GPIO.FALLING, callback = shutdown, bouncetime = 2000)
+GPIO.add_event_detect(button, GPIO.FALLING, callback = reboot, bouncetime = 3000)
 
 # Now wait!
 while 1:
